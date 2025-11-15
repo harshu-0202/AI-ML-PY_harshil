a669253c7e909f2c316919f75d75cd4bcf05ad46
manthan9931 <manthanbhikadiya01@gmail.com>
2025-11-15 10:29:09 +0530

    15/11/2025

---
+++ Day-1/lec-2.4.py
@@ -0,0 +1,31 @@
+# for i in range(1, 13):
+#     if i == 8:
+#         break
+#     print(i)
+
+# for n in range(1, 6):
+#     for i in range(1, 6):
+#         print(i, end=" ")
+#     print()
+
+# for i in range(1, 6):
+#     for j in range(1, i+1):
+#         print(j, end=" ")
+#     print()
+
+# for i in range(1, 6):
+#     for j in range(5, i-1, -1):
+#         print(j, end=" ")
+#     print()
+
+
+# for i in range(5, 0, -1):
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+#     print()
+
+# for i in range(1, 6):
+#     if i==3:
+#         print("* * * * *")
+#     else:
+#         print("*       *")
---
+++ Day-2/lec-3.1.py
@@ -0,0 +1,18 @@
+# a = 4
+# b = 8
+
+# print("Sum of %d and %d is %d" %(a, b, a+b))
+
+# a = "harshil"
+# b = "prajapati"
+
+# print("Sum of %s and %s is %s" %(a, b, a+b))
+
+# a = 4
+# b = 8
+
+# print("Sum of {} and {} is {}" .format(a, b, a+b))
+# print("Sum of {1} and {0} is {2}" .format(a, b, a+b))
+# print("Sum of {num1} and {num2} is {num3}" .format(num1=a, num2=b, num3=a+b))
+
+# print(f"Sum of {a} and {b} is {a+b}")
